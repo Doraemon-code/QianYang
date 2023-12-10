6860ea4c98d14ff3c104f25c25163150b6f656dd
StaceyYang <153273758+StaceyY97@users.noreply.github.com>
2023-12-10 18:30:30 +0800

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 10 00:07:44 2023
+
+@author: Stacey.Yang
+"""
+
+from setuptools import setup
+
+setup(
+    name='CheckListProce',
+    version='1.1',
+    scripts=['CheckListProce.py'],
+)
