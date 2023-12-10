aa6bd01e1f66b6f405d2dd2e6d7dbe05c5f3e0a3
StaceyYang <153273758+StaceyY97@users.noreply.github.com>
2023-12-10 18:56:35 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Dec 10 00:07:44 2023
+Created on Sun Dec 10 17:15:23 2023
 
 @author: Stacey.Yang
 """
 
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='CheckListProce',
+    name='CRFActions',
     version='1.1',
-    scripts=['CheckListProce.py'],
+    packages=find_packages(),  
+    url='https://github.com/Doraemon-code/QianYang',
+    author='Stacey',
+    author_email='yq13408614660@163.com'
 )
