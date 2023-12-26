24e106096d637d744ad5750d505047720f19f9c5
Doraemon-code <51839881+Doraemon-code@users.noreply.github.com>
2023-12-26 23:10:10 +0800

    目前还需要区别方案版本号和CRF版本号

--- CRFActions/ModifyCoverPage.py
+++ CRFActions/ModifyCoverPage.py
@@ -1,35 +1,48 @@
+import os
 import re
-import PyPDF2
+from docx import Document
+from docx.oxml import parse_xml
+from docx2pdf import convert
+from pdf2docx import parse
 
-# 打开PDF文件
-with open('your_file.pdf', 'rb') as file:
-    reader = PyPDF2.PdfFileReader(file)
-    
-    # 选择你想要的页面（例如第一页）
-    page = reader.getPage(0)
-    
-    # 从该页中提取文本
-    text = page.extractText()
-    
-    # 使用正则表达式匹配字符串
-    pattern = r'V\d+\.\d+, \d{2}[A-Za-z]{3}\d{4}'
-    matches = re.findall(pattern, text)
-    
-    # 输出匹配的字符串
-    for match in matches:
-        print(match)
+# 检索PDF文件
+path = r'C:\\Users\Administrator\Desktop'
+pdf_file_path = os.path.join(path, 'CoverPage.pdf')
+new_pdf_file_path = os.path.join(path, 'CoverPage1.pdf')
 
-    # 将匹配的字符串替换为自定义的字符串
-    replacement = 'your_replacement_string'
-    new_text = re.sub(pattern, replacement, text)
+# 检查文件是否存在
+if not os.path.isfile(pdf_file_path):
+    print(f"在路径{path}下面没有CoverPage.pdf")
+else:
+    # 转换PDF到Word
+    docx_file_path = os.path.join(path, 'CoverPage.doc')
+    parse(pdf_file_path, docx_file_path)
 
-# 将新的文本写入新的PDF文件
-with open('new_file.pdf', 'wb') as file:
-    writer = PyPDF2.PdfFileWriter()
-    
-    # 创建一个新的页面并将新的文本添加到该页
-    page = PyPDF2.pdf.PageObject.createBlankPage(None, 210, 297)  # 创建一个空白页
-    page.extractText(new_text)  # 将新的文本添加到该页
-    writer.addPage(page)  # 将该页添加到PDF文件
-    
-    writer.write(file)
+    # 打开docx文件
+    doc = Document(docx_file_path)
+
+    # 匹配正则表达式
+    regex = r'[A-Za-z]+\s?\d\.\d, \d{2}-?[A-Za-z]{3}-?\d{4}'
+    for paragraph in doc.paragraphs:
+        for run in paragraph.runs:
+            text = run.text
+            print(text)
+            if re.search(regex, text):
+                for match in re.finditer(regex, text):
+                    start, end = match.span()
+                    original_text = text[start:end]
+                    replaced_text = "Version 5.1, 20-Dec-2023"
+                    run.text = text.replace(original_text, replaced_text)  # 直接替换文本
+                    print(run.text)
+                    run.bold = False  # 设置加粗
+                    # 设置缩进
+                    element = run._element
+                    if element.getprevious() is None:
+                        element.addprevious(parse_xml(r'<w:ind w:firstLine="720" />'))
+
+    # 保存为新的docx文件
+    new_docx_file_path = os.path.join(path, 'CoverPage1.docx')
+    doc.save(new_docx_file_path)
+
+    # 转换新的docx文件为PDF
+    convert(new_docx_file_path, new_pdf_file_path)
